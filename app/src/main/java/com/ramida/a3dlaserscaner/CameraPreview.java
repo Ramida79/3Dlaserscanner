b9cc87bdceccd4cca5f3fc9d76607142ac39ed24
Ramida <krowicki.pawel@gmail.com>
2016-11-28 21:27:06 +0100

    Picture take added

---
+++ app/src/main/java/com/ramida/a3dlaserscaner/CameraPreview.java
@@ -0,0 +1,120 @@
+package com.ramida.a3dlaserscaner;
+
+/**
+ * Created by Ramida on 2016-11-28.
+ */
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
+       // mCamera.setDisplayOrientation(90);
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            // create the surface and start camera preview
+            if (mCamera == null) {
+
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+            }
+        } catch (IOException e) {
+            Log.d(VIEW_LOG_TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void refreshCamera(Camera camera) {
+        if (mHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e) {
+            // ignore: tried to stop a non-existent preview
+        }
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+        // start preview with new settings
+        setCamera(camera);
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            Log.d(VIEW_LOG_TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+       /* if (isPreviewRunning)
+        {
+            mCamera.stopPreview();
+        }*/
+
+        Camera.Parameters parameters = mCamera.getParameters();
+       // Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+
+      /*  if(display.getRotation() == Surface.ROTATION_0)
+        {
+         //   parameters.setPreviewSize(height, width);
+            mCamera.setDisplayOrientation(90);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_90)
+        {
+         //   parameters.setPreviewSize(width, height);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_180)
+        {
+           // parameters.setPreviewSize(height, width);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_270)
+        {
+          //  parameters.setPreviewSize(width, height);
+            mCamera.setDisplayOrientation(180);
+        }
+
+      //  mCamera.setParameters(parameters);
+        //previewCamera();*/
+
+        mCamera.setDisplayOrientation(90);
+
+        refreshCamera(mCamera);
+    }
+
+    public void setCamera(Camera camera) {
+        //method to set a camera instance
+        mCamera = camera;
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        // mCamera.release();
+
+    }
+}
--- app/src/main/java/com/ramida/a3dlaserscaner/MainActivity.java
+++ app/src/main/java/com/ramida/a3dlaserscaner/MainActivity.java
@@ -1,9 +1,16 @@
 package com.ramida.a3dlaserscaner;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+//import android.graphics.Camera;
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -11,19 +18,49 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.PictureCallback;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private PictureCallback mPicture;
+    private Button capture, switchCamera;
+    private Context myContext;
+    private LinearLayout cameraPreview;
+    private ImageView ImgView;
+    private boolean cameraFront = false;
+    private Bitmap bitmapClone;
+
 
   /*  @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +111,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        myContext = this;
+        initialize();
+
 
 
         hTextView = (TextView) findViewById(R.id.textView);
@@ -141,6 +182,260 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private int findFrontFacingCamera() {
+        int cameraId = -1;
+        // Search for the front facing camera
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            CameraInfo info = new CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
+                cameraId = i;
+                cameraFront = true;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+
+
+    public void onResume() {
+        super.onResume();
+        if (!hasCamera(myContext)) {
+            Toast toast = Toast.makeText(myContext, "Sorry, your phone does not have a camera!", Toast.LENGTH_LONG);
+            toast.show();
+            finish();
+        }
+        if (mCamera == null) {
+            //if the front facing camera does not exist
+            if (findFrontFacingCamera() < 0) {
+                Toast.makeText(this, "No front facing camera found.", Toast.LENGTH_LONG).show();
+                switchCamera.setVisibility(View.GONE);
+            }
+            mCamera = Camera.open(findBackFacingCamera());
+            mPicture = getPictureCallback();
+            mPreview.refreshCamera(mCamera);
+        }
+    }
+    View.OnClickListener switchCameraListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //get the number of cameras
+            int camerasNumber = Camera.getNumberOfCameras();
+            if (camerasNumber > 1) {
+                //release the old camera instance
+                //switch camera, from the front and the back and vice versa
+
+                releaseCamera();
+                chooseCamera();
+            } else {
+                Toast toast = Toast.makeText(myContext, "Sorry, your phone has only one camera!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+    };
+
+    public void chooseCamera() {
+        //if the camera preview is the front
+        if (cameraFront) {
+            int cameraId = findBackFacingCamera();
+            if (cameraId >= 0) {
+                //open the backFacingCamera
+                //set a picture callback
+                //refresh the preview
+
+                mCamera = Camera.open(cameraId);
+                mPicture = getPictureCallback();
+                mPreview.refreshCamera(mCamera);
+            }
+        } else {
+            int cameraId = findFrontFacingCamera();
+            if (cameraId >= 0) {
+                //open the backFacingCamera
+                //set a picture callback
+                //refresh the preview
+
+                mCamera = Camera.open(cameraId);
+                mPicture = getPictureCallback();
+                mPreview.refreshCamera(mCamera);
+            }
+        }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //when on Pause, release camera in order to be used from other applications
+        releaseCamera();
+    }
+
+    private boolean hasCamera(Context context) {
+        //check if the device has camera
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private PictureCallback getPictureCallback() {
+        PictureCallback picture = new PictureCallback() {
+
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+
+                long startTime = System.currentTimeMillis();
+
+
+
+
+
+                ///moje
+
+
+                Bitmap mBitmap = BitmapFactory.decodeByteArray(data, 0 , data.length);
+                bitmapClone = Bitmap.createBitmap(mBitmap.getWidth(), mBitmap.getHeight(),
+                        mBitmap.getConfig());
+                bitmapClone.copy(mBitmap.getConfig(), true);
+
+                int iY = 0;
+                int iX = 0;
+                int iPixel = 0;
+                int iRed = 0;
+                int iGreen = 0;
+                int iBlue = 0;
+                int iRGBAvg = 0;
+                // Gray of image processing.
+
+
+                // The height of the image
+                for ( iY = 0; iY < bitmapClone.getHeight(); iY++ )
+                {
+                    // The width of the image
+                    for ( iX = 0; iX < bitmapClone.getWidth(); iX++ )
+                    {
+                        // To get pixel.
+                        iPixel = mBitmap.getPixel(iX, iY);
+                        // To get value of the red channel.
+                        iRed = Color.red(iPixel);
+                        // To get value of the green channel.
+                        iGreen = Color.green(iPixel);
+                        // To get value of the blue channel.
+                        iBlue = Color.blue(iPixel);
+                        // Compute value of gray.
+                        iRGBAvg = ( iRed + iGreen + iBlue ) / 3;
+                        // Set pixel of gray.
+                        bitmapClone.setPixel(iX, iY, Color.rgb(iRGBAvg, iRGBAvg, iRGBAvg));
+                    }
+                }
+
+                ImgView.setImageBitmap(bitmapClone);
+
+
+                ///moje koniec
+
+
+
+
+                //make a new picture file
+                File pictureFile = getOutputMediaFile();
+
+                if (pictureFile == null) {
+                    return;
+                }
+                try {
+                    //write the file
+                    FileOutputStream fos = new FileOutputStream(pictureFile);
+                    fos.write(data);
+                    fos.close();
+
+                    long difference = System.currentTimeMillis() - startTime;
+
+                    Toast toast = Toast.makeText(myContext, "Picture saved: " + pictureFile.getName()+ " in "+ String.format("%d", TimeUnit.MILLISECONDS.toSeconds(difference)) + " s", Toast.LENGTH_LONG);
+                    toast.show();
+
+                } catch (FileNotFoundException e) {
+                } catch (IOException e) {
+                }
+
+                //refresh camera to continue preview
+                mPreview.refreshCamera(mCamera);
+            }
+        };
+        return picture;
+    }
+
+    private int findBackFacingCamera() {
+        int cameraId = -1;
+        //Search for the back facing camera
+        //get the number of cameras
+        int numberOfCameras = Camera.getNumberOfCameras();
+        //for every camera check
+        for (int i = 0; i < numberOfCameras; i++) {
+            android.hardware.Camera.CameraInfo info = new CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == CameraInfo.CAMERA_FACING_BACK) {
+                cameraId = i;
+                cameraFront = false;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+
+    OnClickListener captrureListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mCamera.takePicture(null, null, mPicture);
+
+        }
+    };
+
+    //make picture and save to a folder
+    private static File getOutputMediaFile() {
+        //make a new file directory inside the "sdcard" folder
+        File mediaStorageDir = new File("/sdcard/", "3DScannerData");
+
+        //if this "JCGCamera folder does not exist
+        if (!mediaStorageDir.exists()) {
+            //if you cannot make this folder return
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+
+        //take the current timeStamp
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        //and make a media file:
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+
+        return mediaFile;
+    }
+
+    private void releaseCamera() {
+        // stop and release camera
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+    public void initialize() {
+        cameraPreview = (LinearLayout) findViewById(R.id.camera_preview);
+        mPreview = new CameraPreview(myContext, mCamera);
+        cameraPreview.addView(mPreview);
+        ImgView = (ImageView)this.findViewById(R.id.imgView);
+
+
+        capture = (Button) findViewById(R.id.button_capture);
+        capture.setOnClickListener(captrureListener);
+
+        switchCamera = (Button) findViewById(R.id.button_ChangeCamera);
+        switchCamera.setOnClickListener(switchCameraListener);
+    }
+
 
     private Runnable hMyTimeTask = new Runnable() {
         @Override
